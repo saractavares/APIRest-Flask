622ddf8885d1a6d72e9da14bea0d02451faa8d52
sara2708 <sara27082011@gmail.com>
2022-01-23 17:29:26 -0300

    add api at repo

---
+++ ma.py
@@ -0,0 +1,6 @@
+"""
+Marshmellow é responsável por serializar as classes do banco de dados e retornar isso como um objeto JSON ou Python na requisição
+"""
+
+from flask_marshmallow import Marshmallow
+ma = Marshmallow()
